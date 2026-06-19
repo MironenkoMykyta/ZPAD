6c5ff9de2494423a72d9967a3eb4147ad0afb803
MironenkoMykyta <mykytamiro-ipt28@lll.kpi.ua>
2026-06-19 17:43:50 +0300

    Add files via upload

---
+++ lab7/src/FrameProcessor.cpp
@@ -0,0 +1,33 @@
+#include "FrameProcessor.hpp"
+#include "KeyProcessor.hpp"
+
+FrameProcessor::FrameProcessor() {
+	detector = new ObjectDetector("/home/nikmir/Desktop/lab7/data/deploy.prototxt", "/home/nikmir/Desktop/lan7/data/mobilenet_iter_73000.caffmodel");
+}
+
+FrameProcessor::~FrameProcessor() {
+	delete detector;
+}
+
+cv::Mat FrameProcessor::process(const cv::Mat& input, ProcessingMode mode, int brightness) {
+    if (input.empty()) return input;
+
+    
+    cv::Mat result;
+    input.copyTo(result);
+    result = detector->detect(result);
+
+    result.convertTo(result, -1, 1, brightness - 50);
+
+    
+    switch (mode) {
+        case ProcessingMode::INVERT: cv::bitwise_not(result, result); break;
+        case ProcessingMode::BLUR: cv::GaussianBlur(result, result, cv::Size(15, 15), 0); break;
+        case ProcessingMode::CANNY: 
+            cv::cvtColor(result, result, cv::COLOR_BGR2GRAY); 
+            cv::Canny(result, result, 100, 200); 
+            break;
+        default: break;
+    }
+    return result;
+}
---
+++ lab7/src/ObjectDetector.cpp
@@ -0,0 +1,12 @@
+#include "ObjectDetector.hpp"
+
+ObjectDetector::ObjectDetector(const std::string& modelPath, const std::string& configPath) {
+    net = cv::dnn::readNetFromCaffe("data/deploy.prototxt", "data/mobilenet_iter_73000.caffmodel");
+}
+
+cv::Mat ObjectDetector::detect(const cv::Mat& frame) {
+    cv::Mat blob = cv::dnn::blobFromImage(frame, 1.0, cv::Size(300, 300), cv::Scalar(104.5, 177.1, 123.9));
+    net.setInput(blob);
+    cv::Mat detections = net.forward();
+    return frame; 
+}
